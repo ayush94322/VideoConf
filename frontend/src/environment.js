037b073514f4574c67764a5da9c9c0059ab753d9
Ayush Yadav <yadavayush94322@gmail.com>
2026-02-03 20:43:56 +0530

    update environment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,5 +1,5 @@
-let IS_PROD = false;
-const server = IS_PROD ?
+let IS_PROD = process.env.NODE_ENV;
+const server = IS_PROD==="production" ?
     "https://videoconfbackend.onrender.com" :
 
     "http://localhost:8000"
